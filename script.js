25394a511f58003901b07d3d2463f1b9ad1082d1
hoangkim <hoangkim1711@gmail.com>
2021-02-22 10:58:42 +0700

    update temp data

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ app.controller('appCtrl', function($scope){
             avtUrl: 'avt/b.jpg'
         },
         {
-            name: 'Cooktail',
+            name: 'Cooktail melal',
             price: 34000,
             avtUrl: 'avt/c.jpg'
         },
@@ -44,6 +44,31 @@ app.controller('appCtrl', function($scope){
             name: 'Capuchino',
             price: 38000,
             avtUrl: 'avt/d.jpg'
+        },
+        {
+            name: 'Cooktail Chanh',
+            price: 34000,
+            avtUrl: 'avt/e.jpg'
+        },
+        {
+            name: 'Ép dưa hấu',
+            price: 32000,
+            avtUrl: 'avt/f.jpg'
+        },
+        {
+            name: 'Trà sữa việt quất',
+            price: 29000,
+            avtUrl: 'avt/g.jpg'
+        },
+        {
+            name: 'Nước cam',
+            price: 41000,
+            avtUrl: 'avt/h.jpg'
+        },
+        {
+            name: 'Ép thơm',
+            price: 24000,
+            avtUrl: 'avt/a.jpg'
         }
     ];
 
@@ -63,14 +88,19 @@ app.controller('appCtrl', function($scope){
     };
 
     // sale history ##########################################################
-    $scope.saleHistory = [];
+    $scope.saleHistory = [
+        {
+            datetime: "2021-1-7 16:34:26",
+            total: 28000
+        }
+    ];
     $scope.saveBill = function(){
         if($scope.totalOrderTemp > 0){
 
             var today = new Date();
-            var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+            var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
             var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-            var dateTimeNOW = date+' '+time;
+            var dateTimeNOW = date + ' ' + time;
 
             $scope.saleHistory.push({
                 datetime: dateTimeNOW,
